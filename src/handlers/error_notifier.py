2459e5edbb8037cc69802ddc1c8732986ec9029a
sinofseven <em.s.00001@gmail.com>
2023-12-09 00:49:32 +0900

    fix topic name

--- src/handlers/error_notifier.py
+++ src/handlers/error_notifier.py
@@ -3,9 +3,7 @@ from dataclasses import dataclass
 from aws_lambda_powertools.utilities.data_classes import (CloudWatchLogsEvent,
                                                           event_source)
 from aws_lambda_powertools.utilities.typing import LambdaContext
-from botocore.client import BaseClient
 
-from common.aws import create_client
 from common.load_environments import load_environments
 from common.logger import create_logger, logging_handler_exception
 
@@ -23,7 +21,6 @@ logger = create_logger(__name__)
 def handler(
     event: CloudWatchLogsEvent,
     context: LambdaContext,
-    client=create_client("s3"),
 ):
     env = load_environments(dataclass_type=EnvironmentVariables)
     logger.debug("env", extra={"data": {"env": env}})
@@ -31,6 +28,3 @@ def handler(
 
     decompressed_log = event.parse_logs_data()
     logger.debug("decompressed", extra={"data": {"decompressed": decompressed_log}})
-
-    resp = client.get_object(Bucket=env.sns_topic_error, Key="data")
-    body = resp["Body"].read()
--- terraform_modules/common/sns.tf
+++ terraform_modules/common/sns.tf
@@ -1,3 +1,4 @@
 resource "aws_sns_topic" "error_topic" {
+  name_prefix = "error_topic"
   display_name = "error_topic"
 }
