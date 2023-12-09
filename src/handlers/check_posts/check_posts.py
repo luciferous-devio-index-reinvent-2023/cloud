3a6f9d42aca582c7e584e74612a0f465041591e1
sinofseven <em.s.00001@gmail.com>
2023-12-09 16:05:10 +0900

    tmp impl check_posts 2

--- src/handlers/check_posts/check_posts.py
+++ src/handlers/check_posts/check_posts.py
@@ -1,13 +1,18 @@
 import json
-from dataclasses import dataclass
-from gzip import decompress
-from typing import Dict, Type, TypeVar
+from dataclasses import asdict, dataclass
+from gzip import compress, decompress
+from hashlib import sha3_256
+from typing import Dict, List, Optional, Set, Tuple, Type, TypeVar
+from urllib.error import HTTPError
 from urllib.parse import urlparse
+from urllib.request import Request
 
 from common.aws import create_client
 from common.http import sec1_http_client
 from common.load_environments import load_environments
 from common.logger import create_logger, logging_function, logging_handler_exception
+
+logger = create_logger(__name__)
 
 
 @dataclass(frozen=True)
@@ -36,10 +41,12 @@ class Author:
 @dataclass(init=False)
 class StoreAuthors:
     map_authors: Dict[str, Author]
+    hash_raw: str
 
     def __init__(self, body: bytes):
         data: dict = json.loads(body)
         self.map_authors = {k: Author(**v) for k, v in data.items()}
+        self.hash_raw = sha3_256(body).hexdigest()
 
     def save_author(self, *, id_author: int) -> Author:
         resp = sec1_http_client(
@@ -62,8 +69,40 @@ class StoreAuthors:
             return author
         return self.save_author(id_author=id_author)
 
+    def convert_to_json_gz(self) -> Tuple[bytes, bool]:
+        authors = {
+            k: self.map_authors[k]
+            for k in sorted(self.map_authors.keys(), key=lambda x: int(x))
+        }
+        binary = json.dumps(authors, ensure_ascii=False, default=asdict).encode()
+        hash_current = sha3_256(binary).hexdigest()
+        result = compress(binary)
+        return result, hash_current != self.hash_raw
 
-logger = create_logger(__name__)
+
+@dataclass(init=False)
+class DevioPost:
+    id: int
+    url: str
+    date: str
+    raw_date: str
+    author_name: str
+    author_url: str
+    author_avatar: str
+
+    @logging_function(logger)
+    def __init__(self, data: dict, store_authors: StoreAuthors):
+        self.id = data["id"]
+        self.url = data["link"]
+        self.raw_date = data["date"]
+        self.date = self.raw_date[:10].replace("-", ".")
+        id_author: int = data["author"]
+        author = store_authors.get_author(id_author=id_author)
+        self.author_name = author.name
+        self.author_url = author.link
+        self.author_avatar = author.avatar
+
+
 T = TypeVar("T")
 
 
@@ -86,6 +125,16 @@ def handler(
         client=client_s3,
         is_gz=True,
     )
+    devio_posts = get_reinvent_posts(store_authors=store_authors)
+
+    compressed_authors, is_flush_authors = store_authors.convert_to_json_gz()
+    if is_flush_authors:
+        put_s3_object(
+            bucket=env.s3_bucket_data,
+            key=env.s3_key_authors,
+            body=compressed_authors,
+            client=client_s3,
+        )
 
 
 @logging_function(logger)
@@ -100,6 +149,11 @@ def get_os3_object(
 
 
 @logging_function(logger)
+def put_s3_object(*, bucket: str, key: str, body: bytes, client):
+    client.put_object(Bucket=bucket, Key=key, Body=body)
+
+
+@logging_function(logger)
 def get_ssm_parameters(*, params: SsmParameters, client) -> SsmParameters:
     resp = client.get_parameters(
         WithDecryption=True,
@@ -110,3 +164,37 @@ def get_ssm_parameters(*, params: SsmParameters, client) -> SsmParameters:
         notion_token=map_params[params.notion_token],
         cloudflare_deploy_hook_url=map_params[params.cloudflare_deploy_hook_url],
     )
+
+
+@logging_function(logger)
+def get_reinvent_posts(*, store_authors: StoreAuthors) -> List[DevioPost]:
+    result = []
+    page = 0
+    while True:
+        page += 1
+        try:
+            resp = sec1_http_client(
+                f"https://dev.classmethod.jp/wp-json/wp/v2/posts?referencecat=10107&page={page}"
+            )
+            data = json.load(resp)
+            result += [DevioPost(data=x, store_authors=store_authors) for x in data]
+        except HTTPError as e:
+            if e.status != 400:
+                raise
+            data = json.load(e.fp)
+            if data["code"] != "rest_post_invalid_page_number":
+                raise
+            return result
+
+
+@logging_function(logger)
+def query_notion_database(*, notion_database_id: str) -> Set[int]:
+    result: Set[int] = set()
+    token: Optional[str] = None
+
+    while token is not None or len(result) == 0:
+        payload = {}
+        if token is not None:
+            payload["start_cursor"] = token
+
+    return result
